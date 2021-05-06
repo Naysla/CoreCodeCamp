c19ae08fbcc6b45c2dbd246cdb2cddef9da171b8
Naysla Tatiana Torres Cortes <60472499+Naysla@users.noreply.github.com>
2021-05-05 20:59:23 -0500

    Adicionar Atributos a consultasdesde Models

---
+++ README.md
@@ -0,0 +1,2 @@
+# CoreApiFundamentals
+This is a practice building an API with ASP.NET Core from the Pluralsight's course by Shawn Wildermuth
--- src/Models/CampModel.cs
+++ src/Models/CampModel.cs
@@ -13,5 +13,15 @@ namespace CoreCodeCamp.Models
         public string Moniker { get; set; }
         public DateTime EventDate { get; set; } = DateTime.MinValue;
         public int Length { get; set; } = 1;
+
+        //Estas propiedades tomadas de la entidad Location se les adiciona el prefijo de esa entidad Location
+        public string LocationVenueName { get; set; }
+        public string LocationAddress1 { get; set; }
+        public string LocationAddress2 { get; set; }
+        public string LocationAddress3 { get; set; }
+        public string LocationCityTown { get; set; }
+        public string LocationStateProvince { get; set; }
+        public string LocationPostalCode { get; set; }
+        public string LocationCountry { get; set; }
     }
 }
